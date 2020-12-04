38d8bf4eef598ae8b90b4141f0fbfe832af9e177
Piotr <piterpawlo16@gmial.com>
2020-12-04 16:24:08 +0100

    lab9

--- app/src/main/java/com/example/foser/MyForegroundService.java
+++ app/src/main/java/com/example/foser/MyForegroundService.java
@@ -5,14 +5,20 @@ import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.app.Service;
+import android.content.Context;
 import android.content.Intent;
 import android.graphics.BitmapFactory;
 import android.os.Build;
+import android.os.Handler;
 import android.os.IBinder;
 import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.annotation.RequiresApi;
+
+import java.sql.Time;
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class MyForegroundService extends Service {
 
@@ -33,13 +39,80 @@ public class MyForegroundService extends Service {
     private String message;
     private Boolean show_time, do_work, double_speed;
 
+    //4
+    private Context ctx;
+    private Intent notificationIntent;
+    private PendingIntent pendingIntent;
+
+    //Stała ta określa co ile milisekund będzie wykonywana praca cykliczna.
+    private final long period = 2000; //2s
+
+    //5 Handler umożliwia komunikacje między wątkami.
+    private int counter;
+    private Timer timer;
+    private TimerTask timerTask;
+
+    final Handler handler = new Handler();
+
+    //Runnable pozwala utworzyć obiekt, który będzie wykonany przez inny wątek.
+    final Runnable runnable = new Runnable() {
+        @Override
+        public void run() {
+
+            //Metoda run uaktualni wyświetlaną notyfikację.
+
+            Notification notification = new Notification.Builder(ctx, CHANNEL_ID)
+                    .setSmallIcon(R.drawable.ic_my_icon)
+                    .setContentTitle(getString(R.string.ser_title))
+                    .setShowWhen(show_time)
+                    .setContentText(message+" "+String.valueOf(counter))
+                    .setLargeIcon(BitmapFactory.decodeResource(getResources(), R.drawable.circle))
+                    .setContentIntent(pendingIntent)
+                    .build();
+
+            NotificationManager manager = getSystemService(NotificationManager.class);
+            manager.notify(1, notification);
+
+        }
+    };
+
     @Override
     public void onCreate() {
         super.onCreate();
+
+        ctx = this;
+        notificationIntent = new Intent(ctx, MainActivity.class);
+        pendingIntent = PendingIntent.getActivity(this, 0, notificationIntent, 0);
+
+        //wyzeruj licznik
+        counter = 0;
+
+        //utwórz obiekt timer-a
+        timer = new Timer();
+
+        //utwórz zadanie timera (TimerTask).
+        timerTask = new TimerTask() {
+            @Override
+            public void run() {
+
+                //zwiększa licznik o 1 i deleguje zadanie do wątku głównego.
+                counter++;
+                handler.post(runnable);
+
+            }
+        };
+
     }
 
     @Override
     public void onDestroy() {
+
+        //zatrzymywała wątek timer-a i umożliwiała jego usuniecie.
+        handler.removeCallbacks(runnable);
+        timer.cancel();
+        timer.purge();
+        timer = null;
+
         super.onDestroy();
     }
 
@@ -62,8 +135,8 @@ public class MyForegroundService extends Service {
 
         createNotificationChannel();
 
-        Intent notificationIntent = new Intent(this, MainActivity.class);
-        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, notificationIntent, 0);
+        //Intent notificationIntent = new Intent(this, MainActivity.class);
+        //PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, notificationIntent, 0);
 
         Notification notification = new Notification.Builder(this, CHANNEL_ID)
                 .setSmallIcon(R.drawable.ic_my_icon)
@@ -83,19 +156,24 @@ public class MyForegroundService extends Service {
 
     private void doWork(){
 
-        try {
-            Thread.sleep(5000);
-        }
-        catch (Exception e){
-            //
+//        try {
+//            Thread.sleep(5000);
+//        }
+//        catch (Exception e){
+//            //
+//        }
+//
+//        String info = "Start working..."
+//                +"\n show_time=" + show_time.toString()
+//                +"\n do_work=" + do_work.toString()
+//                +"\n double_speed=" + double_speed.toString();
+//
+//        Toast.makeText(this, info, Toast.LENGTH_LONG).show();
+
+        if(do_work){
+            timer.schedule(timerTask, 0L, double_speed ? period / 2L : period);
         }
 
-        String info = "Start working..."
-                +"\n show_time=" + show_time.toString()
-                +"\n do_work=" + do_work.toString()
-                +"\n double_speed=" + double_speed.toString();
-
-        Toast.makeText(this, info, Toast.LENGTH_LONG).show();
     }
 
     //metoda tworzy kanał dla Notification:
